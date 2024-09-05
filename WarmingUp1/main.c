72b940511969fa85e57a891ba1b6ce5475319f29
Hyno03 <jenny2117@naver.com>
2024-09-05 22:34:16 +0900

    Simple Code Finish
    procession problem

---
+++ WarmingUp1/main.c
@@ -0,0 +1,329 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <ctype.h>
+
+int main(void)
+{
+    //명령어 입력
+    char order;
+
+    while(order == 'q')
+    {
+        printf("명령어를 입력하세요");
+        scanf("%c", &order);
+        printf("%c 명령어를 입력하셨습니다.\n\n", order);
+
+        //두개의 배열 값 주기, 기본 배열 값만 출력
+        int a[4][4], b[4][4];
+        int c[4][4] = { 0 }, d[4][4] = { 0 };
+        int i, j;
+        int numof2A = 0, numof2B = 0;
+
+        srand(time(NULL));
+        for (i = 0; i < 4; i++)
+        {
+            for (j = 0; j < 4; j++)
+            {
+                a[i][j] = rand() % 3;
+                b[i][j] = rand() % 3;
+                if (a[i][j] == 2)
+                {
+                    numof2A++;
+                    if (numof2A > 2)
+                    {
+                        a[i][j] = rand() % 2;
+                    }
+                }
+                if (b[i][j] == 2)
+                {
+                    numof2B++;
+                    if (numof2B > 2)
+                    {
+                        b[i][j] = rand() % 2;
+                    }
+                }
+                printf("%d ", a[i][j]);
+            }
+            printf("\n");
+        }
+
+
+        //명령어 m (어딘가 이상하니 수정이 필요함!!!!!!!!!!!!!!!!!!!)
+        if (order == 'm')
+        {
+            //두번째 배열 출력
+            printf("   *\n");
+            for (i = 0; i < 4; i++)
+            {
+                for (j = 0; j < 4; j++)
+                {
+                    printf("%d ", b[i][j]);
+                }
+                printf("\n");
+            }
+
+            //행렬의 곱셈 값
+            int k;
+
+            printf("   =\n");
+            for (i = 0; i < 4; i++)
+            {
+                for (j = 0; j < 4; j++)
+                {
+                    for (k = 0; k < 4; k++)
+                    {
+                        c[i][j] += a[i][k] * b[k][i];
+                    }
+                    printf("%d ", c[i][j]);
+                }
+                printf("\n");
+            }
+        }
+
+        //명령어 a
+        if (order == 'a')
+        {
+            //두번째 배열 출력
+            printf("   +\n");
+            for (i = 0; i < 4; i++)
+            {
+                for (j = 0; j < 4; j++)
+                {
+                    printf("%d ", b[i][j]);
+                }
+                printf("\n");
+            }
+
+            //행렬의 덧셈 값
+            printf("   =\n");
+            for (i = 0; i < 4; i++)
+            {
+                for (j = 0; j < 4; j++)
+                {
+                    c[i][j] = a[i][j] + b[i][j];
+                    printf("%d ", c[i][j]);
+                }
+                printf("\n");
+            }
+        }
+
+        //명령어 d
+        if (order == 'd')
+        {
+            //두번째 배열 출력
+            printf("   -\n");
+            for (i = 0; i < 4; i++)
+            {
+                for (j = 0; j < 4; j++)
+                {
+                    printf("%d ", b[i][j]);
+                }
+                printf("\n");
+            }
+
+            //행렬의 뺄셈 값
+            printf("   =\n");
+            for (i = 0; i < 4; i++)
+            {
+                for (j = 0; j < 4; j++)
+                {
+                    c[i][j] = a[i][j] - b[i][j];
+                    printf("%d ", c[i][j]);
+                }
+                printf("\n");
+            }
+        }
+
+        //명령어 r (!!!!!!행렬식의 값에 대해 공부 후 하기!!!!!!!!!!!!)
+        if (order == 'r')
+        {
+            //두번째 배열 출력
+            printf("   *\n");
+            for (i = 0; i < 4; i++)
+            {
+                for (j = 0; j < 4; j++)
+                {
+                    printf("%d ", b[i][j]);
+                }
+                printf("\n");
+            }
+
+            //행렬의 덧셈 값
+            printf("   =\n");
+            for (i = 0; i < 4; i++)
+            {
+                for (j = 0; j < 4; j++)
+                {
+                    c[i][j] = a[i][j] + b[i][j];
+                    printf("%d ", c[i][j]);
+                }
+                printf("\n");
+            }
+        }
+
+        //명령어 t
+        if (order == 't')
+        {
+            //두번째 배열 출력
+            printf("   \n");
+            for (i = 0; i < 4; i++)
+            {
+                for (j = 0; j < 4; j++)
+                {
+                    printf("%d ", b[i][j]);
+                }
+                printf("\n");
+            }
+
+            //a의 전치 행렬 값
+            printf("   =\n");
+            for (i = 0; i < 4; i++)
+            {
+                for (j = 0; j < 4; j++)
+                {
+                    c[i][j] = a[j][i];
+                    printf("%d ", c[i][j]);
+                }
+                printf("\n");
+            }
+
+            //b의 전치 행렬 값
+            printf("   \n");
+            for (i = 0; i < 4; i++)
+            {
+                for (j = 0; j < 4; j++)
+                {
+                    d[i][j] = b[j][i];
+                    printf("%d ", d[i][j]);
+                }
+                printf("\n");
+            }
+        }
+
+        //명령어 e (아직 안함!!!!!!!!!!!!!!!!!!!!)
+        if (order == 'e')
+        {
+            //두번째 배열 출력
+            printf("   \n");
+            for (i = 0; i < 4; i++)
+            {
+                for (j = 0; j < 4; j++)
+                {
+                    printf("%d ", b[i][j]);
+                }
+                printf("\n");
+            }
+
+            //행렬의 덧셈 값
+            printf("   =\n");
+            for (i = 0; i < 4; i++)
+            {
+                for (j = 0; j < 4; j++)
+                {
+                    c[i][j] = a[i][j] + b[i][j];
+                    printf("%d ", c[i][j]);
+                }
+                printf("\n");
+            }
+        }
+
+        //명령어 s
+        if (order == 's')
+        {
+            //두번째 배열 출력
+            printf("   \n");
+            for (i = 0; i < 4; i++)
+            {
+                for (j = 0; j < 4; j++)
+                {
+                    printf("%d ", b[i][j]);
+                }
+                printf("\n");
+            }
+
+            //행렬 랜덤하게 재설정 후 첫번째 배열 출력
+            printf("   =\n");
+            numof2A = 0, numof2B = 0;
+            for (i = 0; i < 4; i++)
+            {
+                for (j = 0; j < 4; j++)
+                {
+                    a[i][j] = rand() % 3;
+                    b[i][j] = rand() % 3;
+                    if (a[i][j] == 2)
+                    {
+                        numof2A++;
+                        if (numof2A > 2)
+                        {
+                            a[i][j] = rand() % 2;
+                        }
+                    }
+                    if (b[i][j] == 2)
+                    {
+                        numof2B++;
+                        if (numof2B > 2)
+                        {
+                            b[i][j] = rand() % 2;
+                        }
+                    }
+                    printf("%d ", a[i][j]);
+                }
+                printf("\n");
+            }
+
+            //두번째 배열 출력
+            printf("   \n");
+            for (i = 0; i < 4; i++)
+            {
+                for (j = 0; j < 4; j++)
+                {
+                    printf("%d ", b[i][j]);
+                }
+                printf("\n");
+            }
+        }
+
+        //숫자 명령어
+        if (isdigit(order))
+        {
+            //입력 받은 값 곱하기
+            printf(" * %c =\n", order);
+            int orderNum = order - '0';
+            for (i = 0; i < 4; i++)
+            {
+                for (j = 0; j < 4; j++)
+                {
+                    c[i][j] = a[i][j] * orderNum;
+                    printf("%d ", c[i][j]);
+                }
+                printf("\n");
+            }
+
+            //두번째 배열 출력
+            printf("\n\n");
+            for (i = 0; i < 4; i++)
+            {
+                for (j = 0; j < 4; j++)
+                {
+                    printf("%d ", b[i][j]);
+                }
+                printf("\n");
+            }
+
+            //두번째 배열 입력 받은 값 곱하기
+            printf(" * %c =\n", order);
+            for (i = 0; i < 4; i++)
+            {
+                for (j = 0; j < 4; j++)
+                {
+                    d[i][j] = b[i][j] * orderNum;
+                    printf("%d ", d[i][j]);
+                }
+                printf("\n");
+            }
+        }
+
+    }
+    
+    return 0;
+}

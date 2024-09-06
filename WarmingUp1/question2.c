f92d44adaa7e716683a05a3182b5532394f6be54
Hyno03 <jenny2117@naver.com>
2024-09-06 19:59:28 +0900

    Code Word Count
    read data.txt and count words line by line

--- WarmingUp1/question2.c
+++ WarmingUp1/question2.c
@@ -1,5 +1,6 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+#include <string.h>
 
 int main() {
     FILE *file = fopen("data.txt", "r");;
@@ -7,7 +8,15 @@ int main() {
     char line[256];
     while (fgets(line, sizeof(line), file))
     {
-        printf("%s", line);
+        printf("%s",line);
+        int wordCount = 0;
+        char *token = strtok(line, " ");
+        while(token != NULL)
+        {
+            wordCount++;
+            token = strtok(NULL, " ");
+        }
+        printf(" = Word Count : %d\n", wordCount);
     }
 
     fclose(file);

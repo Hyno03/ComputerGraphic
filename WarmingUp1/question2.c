7c9ba400b27be150b768a04bff8f5dde7d2a86f7
Hyno03 <jenny2117@naver.com>
2024-09-07 01:45:41 +0900

    Code Order D
    output sentence reverse & each word

--- WarmingUp1/question2.c
+++ WarmingUp1/question2.c
@@ -77,7 +77,59 @@ int main() {
         //명령어 d
         else if(order == 'd')
         {
+            orderCCount++;
+            fseek(file, 0, SEEK_SET);
 
+            //문장 한 줄 전체 거꾸로 출력하기
+            if(orderCCount == 1)
+            {
+                while (fgets(line, sizeof(line), file))
+                {
+                    char *emptyToken[128];
+                    int count = 0;
+
+                    char *token = strtok(line, " ");
+                    while(token != NULL)
+                    {
+                        if(strlen(token) > 0)
+                        {
+                            emptyToken[count++] = token;
+                        }
+                        token = strtok(NULL, " ");
+                    }
+                    for(int i = count - 1; i >= 0; i--)
+                    {
+                        int len = strlen(emptyToken[i]);
+                        for(int j = len; j >=0; j--)
+                        {
+                            printf("%c", emptyToken[i][j]);
+                        }
+                        printf(" ");
+                    }
+                    printf("\n");
+                }
+            }
+            //원래대로 출력
+            else
+            {
+                while (fgets(line, sizeof(line), file))
+                {
+                    char *token = strtok(line, " ");
+                    while(token != NULL)
+                    {
+                        printf("%s ", token);
+                        token = strtok(NULL, " ");
+                    }
+                    printf("\n");
+                }
+                orderCCount = 0;
+            }
+        }
+
+        //명령어 e
+        if(order == 'e')
+        {
+                
         }
 
         //명령어 q
@@ -87,6 +139,7 @@ int main() {
             return 0;
         }
 
+        //지정된 명령어를 입력하지 않았을 때
         else
         {
             printf("No Order Exist.");

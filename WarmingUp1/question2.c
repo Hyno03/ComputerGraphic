a511c03b52823cfcd5bfb277d71e2734489faa2f
Hyno03 <jenny2117@naver.com>
2024-09-09 21:55:34 +0900

    Code Order R & Modify Codes

--- WarmingUp1/question2.c
+++ WarmingUp1/question2.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
+#include <windows.h>
 
 #define RESET "\x1b[0m"
 #define RED "\x1b[31m"
@@ -11,7 +12,7 @@ int main() {
 
     char line[256];
     char order;
-    int orderCCount = 0;
+    int orderCCount = 0, orderDCount = 0, orderECount = 0, orderFCount = 0, orderGCount = 0, orderHCount = 0, orderRCount = 0, orderSCount = 0;
 
     while (1)
     {
@@ -53,7 +54,7 @@ int main() {
                             }
                             else
                             {
-                                printf("%s",token);
+                                printf(" %s",token);
                             }
                         }
                         token = strtok(NULL, " ");
@@ -88,11 +89,11 @@ int main() {
         //명령어 d
         else if(order == 'd')
         {
-            orderCCount++;
+            orderDCount++;
             fseek(file, 0, SEEK_SET);
 
             //문장 한 줄 전체 거꾸로 출력하기
-            if(orderCCount == 1)
+            if(orderDCount == 1)
             {
                 while (fgets(line, sizeof(line), file))
                 {
@@ -140,18 +141,18 @@ int main() {
                         token = strtok(NULL, " ");
                     }
                 }
-                orderCCount = 0;
+                orderDCount = 0;
             }
         }
 
         //명령어 e
         if(order == 'e')
         {
-            orderCCount++;
+            orderECount++;
             fseek(file, 0, SEEK_SET);
 
             //3문자 뒤에 @@ 붙이기
-            if(orderCCount == 1)
+            if(orderECount == 1)
             {
                 while (fgets(line, sizeof(line), file))
                 {
@@ -196,18 +197,18 @@ int main() {
                         token = strtok(NULL, " ");
                     }
                 }
-                orderCCount = 0;
+                orderECount = 0;
             }
         }
 
         //명령어 f
         if(order == 'f')
         {
-            orderCCount++;
+            orderFCount++;
             fseek(file, 0, SEEK_SET);
 
             //공백을 기준으로 단어 거꾸로 출력
-            if(orderCCount == 1)
+            if(orderFCount == 1)
             {
                 while (fgets(line, sizeof(line), file))
                 {
@@ -258,7 +259,7 @@ int main() {
                         token = strtok(NULL, " ");
                     }
                 }
-                orderCCount = 0;
+                orderFCount = 0;
             }
         }
 
@@ -266,11 +267,11 @@ int main() {
         if (order == 'g')
         {
             char orignalWord[256], changeWord[256];
-            orderCCount++;
+            orderGCount++;
             fseek(file, 0, SEEK_SET);
 
             //문자 내부의 특정 문자를 다른 문자로 바꾸기 (바꿀 문자와 입력할 문자 입력 받음)
-            if (orderCCount == 1)
+            if (orderGCount == 1)
             {
                 //원래 텍스트 출력
                 while (fgets(line, sizeof(line), file))
@@ -342,18 +343,18 @@ int main() {
                         token = strtok(NULL, " ");
                     }
                 }
-                orderCCount = 0;
+                orderGCount = 0;
             }
         }
 
         //명령어 h
         if (order == 'h')
         {
-            orderCCount++;
+            orderHCount++;
             fseek(file, 0, SEEK_SET);
 
             //각 줄의 단어의 개수
-            if (orderCCount == 1)
+            if (orderHCount == 1)
             {
                  while (fgets(line, sizeof(line), file))
                  {
@@ -390,63 +391,93 @@ int main() {
                         token = strtok(NULL, " ");
                     }
                 }
-                orderCCount = 0;
+                orderHCount = 0;
             }
         }
 
-        //명령어 r
+        // 명령어 r
         if (order == 'r')
         {
-            orderCCount++;
+            orderRCount++;
             fseek(file, 0, SEEK_SET);
 
-            //오름차순으로 출력
-            if (orderCCount == 1)
-            {
-                while (fgets(line, sizeof(line), file))
-                {
-                    int wordCount = 0;
-                    char* token = strtok(line, " ");
-                    while (token != NULL)
-                    {
-                        wordCount++;
-                        token = strtok(NULL, " ");
-                    }
-                    for (int i = 0; i <= wordCount; i++)
-                    {
+            char lines[100][256];
+            int wordCounts[100];
+            int lineCount = 0;
 
+            // 파일의 모든 줄을 읽고 단어 수를 세어서 저장
+            while (fgets(line, sizeof(line), file))
+            {
+                strcpy(lines[lineCount], line);
+
+                int wordCount = 0;
+                char* token = strtok(line, " ");
+                while (token != NULL)
+                {
+                    wordCount++;
+                    token = strtok(NULL, " ");
+                }
+                wordCounts[lineCount] = wordCount;
+                lineCount++;
+            }
+
+            // 오름차순으로 정렬
+            if (orderRCount == 1)
+            {
+                for (int i = 0; i < lineCount - 1; i++)
+                {
+                    for (int j = i + 1; j < lineCount; j++)
+                    {
+                        if (wordCounts[i] > wordCounts[j])
+                        {
+                            // 단어 수 교환
+                            int tempCount = wordCounts[i];
+                            wordCounts[i] = wordCounts[j];
+                            wordCounts[j] = tempCount;
+
+                            // 줄 교환
+                            char tempLine[256];
+                            strcpy(tempLine, lines[i]);
+                            strcpy(lines[i], lines[j]);
+                            strcpy(lines[j], tempLine);
+                        }
+                    }
+                }
+            }
+            // 내림차순으로 정렬
+            else if (orderRCount == 2)
+            {
+                for (int i = 0; i < lineCount - 1; i++)
+                {
+                    for (int j = i + 1; j < lineCount; j++)
+                    {
+                        if (wordCounts[i] < wordCounts[j])
+                        {
+                            // 단어 수 교환
+                            int tempCount = wordCounts[i];
+                            wordCounts[i] = wordCounts[j];
+                            wordCounts[j] = tempCount;
+
+                            // 줄 교환
+                            char tempLine[256];
+                            strcpy(tempLine, lines[i]);
+                            strcpy(lines[i], lines[j]);
+                            strcpy(lines[j], tempLine);
+                        }
                     }
                 }
             }
 
-            //내림차순으로 출력
-            else if (orderCCount == 2)
+            // 정렬된 결과 출력 (각 줄의 단어 수도 함께 출력)
+            for (int i = 0; i < lineCount; i++)
             {
-
+                printf("%s", lines[i]);
             }
 
-            //원래대로 출력
-            else
+            // orderRCount를 초기화
+            if (orderRCount > 2)
             {
-                while (fgets(line, sizeof(line), file))
-                {
-                    char* token = strtok(line, " ");
-                    int firstWord = 1;
-                    while (token != NULL)
-                    {
-                        if (firstWord)
-                        {
-                            printf("%s", token);
-                            firstWord = 0;
-                        }
-                        else
-                        {
-                            printf(" %s", token);
-                        }
-                        token = strtok(NULL, " ");
-                    }
-                }
-                orderCCount = 0;
+                orderRCount = 0;
             }
         }
 
@@ -455,11 +486,11 @@ int main() {
         if (order == 's')
         {
             char changeWord[256];
-            orderCCount++;
+            orderSCount++;
             fseek(file, 0, SEEK_SET);
 
             //바꾸기로 입력한 단어 대소문자 구분 없이 색 바꾸기
-            if (orderCCount == 1)
+            if (orderSCount == 1)
             {
                 //원래 텍스트 출력
                 while (fgets(line, sizeof(line), file))
@@ -468,9 +499,10 @@ int main() {
                 }
 
                 //색을 바꿀 단어 입력
-                printf("\nEnter the word to change");
+                printf("\nEnter the word to change color ");
                 scanf("%s", changeWord);
 
+                fseek(file, 0, SEEK_SET);
                 //변환 코드
                 while (fgets(line, sizeof(line), file))
                 {
@@ -478,7 +510,13 @@ int main() {
                     int firstWord = 1;
                     while (token != NULL)
                     {
-                        if (_strcmpi(token, changeWord) == 0)
+                        char tempToken[256], tempChangeWord[256];
+                        strcpy(tempToken, token);
+                        strcpy(tempChangeWord, changeWord);
+                        for (int i = 0; tempToken[i]; i++) tempToken[i] = tolower(tempToken[i]);
+                        for (int i = 0; tempChangeWord[i]; i++) tempChangeWord[i] = tolower(tempChangeWord[i]);
+
+                        if (strcmp(tempToken, tempChangeWord) == 0)
                         {
                             if (firstWord)
                             {
@@ -527,7 +565,7 @@ int main() {
                         token = strtok(NULL, " ");
                     }
                 }
-                orderCCount = 0;
+                orderSCount = 0;
             }
         }
 

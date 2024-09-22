083afc622e6d80ee549d6837dade925de8008bd3
Hyno03 <jenny2117@naver.com>
2024-09-23 03:20:51 +0900

    Modify Question 6

--- WarmingUp1/question3.c
+++ WarmingUp1/question3.c
@@ -1,304 +1,304 @@
-#define _CRT_SECURE_NO_WARNINGS
-#include <stdio.h>
-#include <math.h>
-
-#define MAX_POINTS 20
-
-// 점 구조체 정의
-typedef struct {
-    int x;
-    int y;
-    int z;
-} Point;
-
-// 점 리스트 구조체 정의
-typedef struct {
-    Point points[MAX_POINTS];
-    int size;
-} PointList;
-
-Point original[MAX_POINTS];  // 원래 인덱스를 저장하는 배열
-int sortAFlag = 0;           // 오름차순 정렬 여부를 관리하는 플래그 (1 = 정렬됨, 2 = 원래 상태로 복원)
-int sortSFlag = 0;           // 내림차순 정렬 여부를 관리하는 플래그 (1 = 정렬됨, 2 = 원래 상태로 복원)
-
-// 함수 선언
-void initPointList(PointList* list);
-void addPointAtTop(PointList* list, int x, int y, int z);
-void addPointAtBottom(PointList* list, int x, int y, int z);
-void removePointAtTop(PointList* list);
-void removePointAtBottom(PointList* list);
-void printPoints(const PointList* list);
-void clearPoints(PointList* list);
-int countPoints(const PointList* list);
-void findFurthestPoint(const PointList* list);
-void findClosestPoint(const PointList* list);
-double calculateDistance(int x, int y, int z);
-void sortPointsByDistance(PointList* list, int ascending);
-void restoreOriginalOrder(PointList* list);
-
-int main() {
-    PointList list;
-    initPointList(&list);
-
-    char command;
-    int x, y, z;
-
-    while (1) {
-        printf("\n명령어 입력: ");
-        scanf(" %c", &command);  // 공백 추가로 버퍼 문제 방지
-
-        if (command == '+') {
-            if (list.size >= MAX_POINTS) {
-                printf("점 데이터가 가득 찼습니다.\n");
-                continue;
-            }
-            printf("x, y, z 값을 입력하세요: ");
-            scanf("%d %d %d", &x, &y, &z);
-            addPointAtTop(&list, x, y, z);
-        }
-        else if (command == 'e') {
-            if (list.size >= MAX_POINTS) {
-                printf("점 데이터가 가득 찼습니다.\n");
-                continue;
-            }
-            printf("x, y, z 값을 입력하세요: ");
-            scanf("%d %d %d", &x, &y, &z);
-            addPointAtBottom(&list, x, y, z);
-        }
-        else if (command == '-') {
-            if (list.size == 0) {
-                printf("삭제할 점이 없습니다.\n");
-            }
-            else {
-                removePointAtTop(&list);
-            }
-        }
-        else if (command == 'd') {
-            if (list.size == 0) {
-                printf("삭제할 점이 없습니다.\n");
-            }
-            else {
-                removePointAtBottom(&list);
-            }
-        }
-        else if (command == 'l') {
-            printf("리스트에 저장된 점의 개수: %d\n", countPoints(&list));
-        }
-        else if (command == 'c') {
-            clearPoints(&list);
-            printf("리스트가 초기화되었습니다.\n");
-            printPoints(&list);  // 명령어 c 후 리스트 출력
-        }
-        else if (command == 'm') {
-            findFurthestPoint(&list);
-        }
-        else if (command == 'n') {
-            findClosestPoint(&list);
-        }
-        else if (command == 'a') {
-            if (sortAFlag == 1) {
-                restoreOriginalOrder(&list);
-                sortAFlag = 0;
-                printf("리스트가 원래 순서로 복원되었습니다.\n");
-            }
-            else {
-                sortPointsByDistance(&list, 1); // 오름차순 정렬
-                sortAFlag = 1;
-                sortSFlag = 0;  // 's'를 눌렀다가 다시 'a'를 누르면 복원 없이 정렬
-            }
-        }
-        else if (command == 's') {
-            if (sortSFlag == 1) {
-                restoreOriginalOrder(&list);
-                sortSFlag = 0;
-                printf("리스트가 원래 순서로 복원되었습니다.\n");
-            }
-            else {
-                sortPointsByDistance(&list, 0); // 내림차순 정렬
-                sortSFlag = 1;
-                sortAFlag = 0;  // 'a'를 눌렀다가 다시 's'를 누르면 복원 없이 정렬
-            }
-        }
-        else if (command == 'q') {
-            printf("프로그램 종료.\n");
-            break;
-        }
-        else {
-            printf("잘못된 명령어입니다.\n");
-        }
-
-        // 'l', 'm', 'n' 명령어는 전체 리스트를 출력하지 않음
-        if (command != 'l' && command != 'm' && command != 'n') {
-            printPoints(&list);  // 명령어 완료 후 전체 리스트 출력
-        }
-    }
-
-    return 0;
-}
-
-// 점 리스트 초기화
-void initPointList(PointList* list) {
-    list->size = 0;
-    for (int i = 0; i < MAX_POINTS; i++) {
-        list->points[i].x = 0;
-        list->points[i].y = 0;
-        list->points[i].z = 0;
-        original[i] = list->points[i];  // 원래 인덱스에 초기값 저장
-    }
-}
-
-// 맨 위에 점 추가 (위로 쌓이게)
-void addPointAtTop(PointList* list, int x, int y, int z) {
-    for (int i = list->size; i > 0; i--) {
-        list->points[i] = list->points[i - 1];
-    }
-    list->points[0].x = x;
-    list->points[0].y = y;
-    list->points[0].z = z;
-    list->size++;
-    printf("맨 위에 (x: %d, y: %d, z: %d) 추가됨.\n", x, y, z);
-    for (int i = 0; i < list->size; i++) {
-        original[i] = list->points[i];  // 추가 시 원래 인덱스 갱신
-    }
-}
-
-// 맨 아래에 점 추가
-void addPointAtBottom(PointList* list, int x, int y, int z) {
-    list->points[list->size].x = x;
-    list->points[list->size].y = y;
-    list->points[list->size].z = z;
-    list->size++;
-    printf("맨 아래에 (x: %d, y: %d, z: %d) 추가됨.\n", x, y, z);
-    for (int i = 0; i < list->size; i++) {
-        original[i] = list->points[i];  // 추가 시 원래 인덱스 갱신
-    }
-}
-
-// 맨 위에 있는 점 삭제 (빈 자리 유지)
-void removePointAtTop(PointList* list) {
-    list->points[0].x = NULL;
-    list->points[0].y = NULL;
-    list->points[0].z = NULL;
-    printf("맨 위의 점 삭제됨.\n");
-    // 크기 줄이지 않음, 대신 빈 자리만 남김
-}
-
-// 맨 아래에 있는 점 삭제
-void removePointAtBottom(PointList* list) {
-    list->points[list->size - 1].x = 0;
-    list->points[list->size - 1].y = 0;
-    list->points[list->size - 1].z = 0;
-    list->size--;
-    printf("맨 아래의 점 삭제됨.\n");
-}
-
-// 리스트 전체 출력
-void printPoints(const PointList* list) {
-    printf("\n현재 리스트 상태:\n");
-    for (int i = 0; i < MAX_POINTS; i++) {
-        if (i < list->size && (list->points[i].x != NULL || list->points[i].y != NULL || list->points[i].z != NULL)) {
-            printf("인덱스 %d: (x: %d, y: %d, z: %d)\n", i, list->points[i].x, list->points[i].y, list->points[i].z);
-        }
-        else {
-            printf("인덱스 %d: 비어있음\n", i);
-        }
-    }
-}
-
-// 리스트 비우기
-void clearPoints(PointList* list) {
-    list->size = 0;
-    for (int i = 0; i < MAX_POINTS; i++) {
-        original[i].x = original[i].y = original[i].z = 0;
-    }
-}
-
-// 리스트의 점 개수 반환
-int countPoints(const PointList* list) {
-    for (int i = 0; i < MAX_POINTS; i++) {
-        if (list->points[i].x == NULL || list->points[i].y == NULL || list->points[i].z == NULL) {
-            return list->size - 1;
-        }
-        else
-        {
-            return list->size;
-        }
-    }
-}
-
-// 원점에서 가장 먼 점 찾기
-void findFurthestPoint(const PointList* list) {
-    if (list->size == 0) {
-        printf("점 데이터가 없습니다.\n");
-        return;
-    }
-
-    int furthestIndex = 0;
-    double maxDistance = calculateDistance(list->points[0].x, list->points[0].y, list->points[0].z);
-
-    for (int i = 1; i < list->size; i++) {
-        double distance = calculateDistance(list->points[i].x, list->points[i].y, list->points[i].z);
-        if (distance > maxDistance) {
-            maxDistance = distance;
-            furthestIndex = i;
-        }
-    }
-
-    printf("가장 먼 점: (x: %d, y: %d, z: %d)\n", list->points[furthestIndex].x, list->points[furthestIndex].y, list->points[furthestIndex].z);
-}
-
-// 원점에서 가장 가까운 점 찾기
-void findClosestPoint(const PointList* list) {
-    if (list->size == 0) {
-        printf("점 데이터가 없습니다.\n");
-        return;
-    }
-
-    int closestIndex = 0;
-    double minDistance = calculateDistance(list->points[0].x, list->points[0].y, list->points[0].z);
-
-    for (int i = 1; i < list->size; i++) {
-        double distance = calculateDistance(list->points[i].x, list->points[i].y, list->points[i].z);
-        if (distance < minDistance) {
-            minDistance = distance;
-            closestIndex = i;
-        }
-    }
-
-    printf("가장 가까운 점: (x: %d, y: %d, z: %d)\n", list->points[closestIndex].x, list->points[closestIndex].y, list->points[closestIndex].z);
-}
-
-// 원점으로부터의 거리 계산
-double calculateDistance(int x, int y, int z) {
-    return sqrt(x * x + y * y + z * z);
-}
-
-// 거리 기준으로 정렬 (ascending = 1: 오름차순, ascending = 0: 내림차순)
-void sortPointsByDistance(PointList* list, int ascending) {
-    for (int i = 0; i < list->size - 1; i++) {
-        for (int j = i + 1; j < list->size; j++) {
-            double dist1 = calculateDistance(list->points[i].x, list->points[i].y, list->points[i].z);
-            double dist2 = calculateDistance(list->points[j].x, list->points[j].y, list->points[j].z);
-
-            if ((ascending && dist1 > dist2) || (!ascending && dist1 < dist2)) {
-                Point temp = list->points[i];
-                list->points[i] = list->points[j];
-                list->points[j] = temp;
-            }
-        }
-    }
-
-    printf("리스트가 %s으로 정렬되었습니다.\n", ascending ? "오름차순" : "내림차순");
-}
-
-// 원래 순서로 복원
-void restoreOriginalOrder(PointList* list) {
-    for (int i = 0; i < MAX_POINTS; i++) {
-        if (i < list->size && (list->points[i].x != NULL || list->points[i].y != NULL || list->points[i].z != NULL)) {
-            printf("인덱스 %d: (x: %d, y: %d, z: %d)\n", i, list->points[i].x, list->points[i].y, list->points[i].z);
-        }
-        else {
-            printf("인덱스 %d: 비어있음\n", i);
-        }
-    }
-}
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//#include <math.h>
+//
+//#define MAX_POINTS 20
+//
+//// 점 구조체 정의
+//typedef struct {
+//    int x;
+//    int y;
+//    int z;
+//} Point;
+//
+//// 점 리스트 구조체 정의
+//typedef struct {
+//    Point points[MAX_POINTS];
+//    int size;
+//} PointList;
+//
+//Point original[MAX_POINTS];  // 원래 인덱스를 저장하는 배열
+//int sortAFlag = 0;           // 오름차순 정렬 여부를 관리하는 플래그 (1 = 정렬됨, 2 = 원래 상태로 복원)
+//int sortSFlag = 0;           // 내림차순 정렬 여부를 관리하는 플래그 (1 = 정렬됨, 2 = 원래 상태로 복원)
+//
+//// 함수 선언
+//void initPointList(PointList* list);
+//void addPointAtTop(PointList* list, int x, int y, int z);
+//void addPointAtBottom(PointList* list, int x, int y, int z);
+//void removePointAtTop(PointList* list);
+//void removePointAtBottom(PointList* list);
+//void printPoints(const PointList* list);
+//void clearPoints(PointList* list);
+//int countPoints(const PointList* list);
+//void findFurthestPoint(const PointList* list);
+//void findClosestPoint(const PointList* list);
+//double calculateDistance(int x, int y, int z);
+//void sortPointsByDistance(PointList* list, int ascending);
+//void restoreOriginalOrder(PointList* list);
+//
+//int main() {
+//    PointList list;
+//    initPointList(&list);
+//
+//    char command;
+//    int x, y, z;
+//
+//    while (1) {
+//        printf("\n명령어 입력: ");
+//        scanf(" %c", &command);  // 공백 추가로 버퍼 문제 방지
+//
+//        if (command == '+') {
+//            if (list.size >= MAX_POINTS) {
+//                printf("점 데이터가 가득 찼습니다.\n");
+//                continue;
+//            }
+//            printf("x, y, z 값을 입력하세요: ");
+//            scanf("%d %d %d", &x, &y, &z);
+//            addPointAtTop(&list, x, y, z);
+//        }
+//        else if (command == 'e') {
+//            if (list.size >= MAX_POINTS) {
+//                printf("점 데이터가 가득 찼습니다.\n");
+//                continue;
+//            }
+//            printf("x, y, z 값을 입력하세요: ");
+//            scanf("%d %d %d", &x, &y, &z);
+//            addPointAtBottom(&list, x, y, z);
+//        }
+//        else if (command == '-') {
+//            if (list.size == 0) {
+//                printf("삭제할 점이 없습니다.\n");
+//            }
+//            else {
+//                removePointAtTop(&list);
+//            }
+//        }
+//        else if (command == 'd') {
+//            if (list.size == 0) {
+//                printf("삭제할 점이 없습니다.\n");
+//            }
+//            else {
+//                removePointAtBottom(&list);
+//            }
+//        }
+//        else if (command == 'l') {
+//            printf("리스트에 저장된 점의 개수: %d\n", countPoints(&list));
+//        }
+//        else if (command == 'c') {
+//            clearPoints(&list);
+//            printf("리스트가 초기화되었습니다.\n");
+//            printPoints(&list);  // 명령어 c 후 리스트 출력
+//        }
+//        else if (command == 'm') {
+//            findFurthestPoint(&list);
+//        }
+//        else if (command == 'n') {
+//            findClosestPoint(&list);
+//        }
+//        else if (command == 'a') {
+//            if (sortAFlag == 1) {
+//                restoreOriginalOrder(&list);
+//                sortAFlag = 0;
+//                printf("리스트가 원래 순서로 복원되었습니다.\n");
+//            }
+//            else {
+//                sortPointsByDistance(&list, 1); // 오름차순 정렬
+//                sortAFlag = 1;
+//                sortSFlag = 0;  // 's'를 눌렀다가 다시 'a'를 누르면 복원 없이 정렬
+//            }
+//        }
+//        else if (command == 's') {
+//            if (sortSFlag == 1) {
+//                restoreOriginalOrder(&list);
+//                sortSFlag = 0;
+//                printf("리스트가 원래 순서로 복원되었습니다.\n");
+//            }
+//            else {
+//                sortPointsByDistance(&list, 0); // 내림차순 정렬
+//                sortSFlag = 1;
+//                sortAFlag = 0;  // 'a'를 눌렀다가 다시 's'를 누르면 복원 없이 정렬
+//            }
+//        }
+//        else if (command == 'q') {
+//            printf("프로그램 종료.\n");
+//            break;
+//        }
+//        else {
+//            printf("잘못된 명령어입니다.\n");
+//        }
+//
+//        // 'l', 'm', 'n' 명령어는 전체 리스트를 출력하지 않음
+//        if (command != 'l' && command != 'm' && command != 'n') {
+//            printPoints(&list);  // 명령어 완료 후 전체 리스트 출력
+//        }
+//    }
+//
+//    return 0;
+//}
+//
+//// 점 리스트 초기화
+//void initPointList(PointList* list) {
+//    list->size = 0;
+//    for (int i = 0; i < MAX_POINTS; i++) {
+//        list->points[i].x = 0;
+//        list->points[i].y = 0;
+//        list->points[i].z = 0;
+//        original[i] = list->points[i];  // 원래 인덱스에 초기값 저장
+//    }
+//}
+//
+//// 맨 위에 점 추가 (위로 쌓이게)
+//void addPointAtTop(PointList* list, int x, int y, int z) {
+//    for (int i = list->size; i > 0; i--) {
+//        list->points[i] = list->points[i - 1];
+//    }
+//    list->points[0].x = x;
+//    list->points[0].y = y;
+//    list->points[0].z = z;
+//    list->size++;
+//    printf("맨 위에 (x: %d, y: %d, z: %d) 추가됨.\n", x, y, z);
+//    for (int i = 0; i < list->size; i++) {
+//        original[i] = list->points[i];  // 추가 시 원래 인덱스 갱신
+//    }
+//}
+//
+//// 맨 아래에 점 추가
+//void addPointAtBottom(PointList* list, int x, int y, int z) {
+//    list->points[list->size].x = x;
+//    list->points[list->size].y = y;
+//    list->points[list->size].z = z;
+//    list->size++;
+//    printf("맨 아래에 (x: %d, y: %d, z: %d) 추가됨.\n", x, y, z);
+//    for (int i = 0; i < list->size; i++) {
+//        original[i] = list->points[i];  // 추가 시 원래 인덱스 갱신
+//    }
+//}
+//
+//// 맨 위에 있는 점 삭제 (빈 자리 유지)
+//void removePointAtTop(PointList* list) {
+//    list->points[0].x = NULL;
+//    list->points[0].y = NULL;
+//    list->points[0].z = NULL;
+//    printf("맨 위의 점 삭제됨.\n");
+//    // 크기 줄이지 않음, 대신 빈 자리만 남김
+//}
+//
+//// 맨 아래에 있는 점 삭제
+//void removePointAtBottom(PointList* list) {
+//    list->points[list->size - 1].x = 0;
+//    list->points[list->size - 1].y = 0;
+//    list->points[list->size - 1].z = 0;
+//    list->size--;
+//    printf("맨 아래의 점 삭제됨.\n");
+//}
+//
+//// 리스트 전체 출력
+//void printPoints(const PointList* list) {
+//    printf("\n현재 리스트 상태:\n");
+//    for (int i = 0; i < MAX_POINTS; i++) {
+//        if (i < list->size && (list->points[i].x != NULL || list->points[i].y != NULL || list->points[i].z != NULL)) {
+//            printf("인덱스 %d: (x: %d, y: %d, z: %d)\n", i, list->points[i].x, list->points[i].y, list->points[i].z);
+//        }
+//        else {
+//            printf("인덱스 %d: 비어있음\n", i);
+//        }
+//    }
+//}
+//
+//// 리스트 비우기
+//void clearPoints(PointList* list) {
+//    list->size = 0;
+//    for (int i = 0; i < MAX_POINTS; i++) {
+//        original[i].x = original[i].y = original[i].z = 0;
+//    }
+//}
+//
+//// 리스트의 점 개수 반환
+//int countPoints(const PointList* list) {
+//    for (int i = 0; i < MAX_POINTS; i++) {
+//        if (list->points[i].x == NULL || list->points[i].y == NULL || list->points[i].z == NULL) {
+//            return list->size - 1;
+//        }
+//        else
+//        {
+//            return list->size;
+//        }
+//    }
+//}
+//
+//// 원점에서 가장 먼 점 찾기
+//void findFurthestPoint(const PointList* list) {
+//    if (list->size == 0) {
+//        printf("점 데이터가 없습니다.\n");
+//        return;
+//    }
+//
+//    int furthestIndex = 0;
+//    double maxDistance = calculateDistance(list->points[0].x, list->points[0].y, list->points[0].z);
+//
+//    for (int i = 1; i < list->size; i++) {
+//        double distance = calculateDistance(list->points[i].x, list->points[i].y, list->points[i].z);
+//        if (distance > maxDistance) {
+//            maxDistance = distance;
+//            furthestIndex = i;
+//        }
+//    }
+//
+//    printf("가장 먼 점: (x: %d, y: %d, z: %d)\n", list->points[furthestIndex].x, list->points[furthestIndex].y, list->points[furthestIndex].z);
+//}
+//
+//// 원점에서 가장 가까운 점 찾기
+//void findClosestPoint(const PointList* list) {
+//    if (list->size == 0) {
+//        printf("점 데이터가 없습니다.\n");
+//        return;
+//    }
+//
+//    int closestIndex = 0;
+//    double minDistance = calculateDistance(list->points[0].x, list->points[0].y, list->points[0].z);
+//
+//    for (int i = 1; i < list->size; i++) {
+//        double distance = calculateDistance(list->points[i].x, list->points[i].y, list->points[i].z);
+//        if (distance < minDistance) {
+//            minDistance = distance;
+//            closestIndex = i;
+//        }
+//    }
+//
+//    printf("가장 가까운 점: (x: %d, y: %d, z: %d)\n", list->points[closestIndex].x, list->points[closestIndex].y, list->points[closestIndex].z);
+//}
+//
+//// 원점으로부터의 거리 계산
+//double calculateDistance(int x, int y, int z) {
+//    return sqrt(x * x + y * y + z * z);
+//}
+//
+//// 거리 기준으로 정렬 (ascending = 1: 오름차순, ascending = 0: 내림차순)
+//void sortPointsByDistance(PointList* list, int ascending) {
+//    for (int i = 0; i < list->size - 1; i++) {
+//        for (int j = i + 1; j < list->size; j++) {
+//            double dist1 = calculateDistance(list->points[i].x, list->points[i].y, list->points[i].z);
+//            double dist2 = calculateDistance(list->points[j].x, list->points[j].y, list->points[j].z);
+//
+//            if ((ascending && dist1 > dist2) || (!ascending && dist1 < dist2)) {
+//                Point temp = list->points[i];
+//                list->points[i] = list->points[j];
+//                list->points[j] = temp;
+//            }
+//        }
+//    }
+//
+//    printf("리스트가 %s으로 정렬되었습니다.\n", ascending ? "오름차순" : "내림차순");
+//}
+//
+//// 원래 순서로 복원
+//void restoreOriginalOrder(PointList* list) {
+//    for (int i = 0; i < MAX_POINTS; i++) {
+//        if (i < list->size && (list->points[i].x != NULL || list->points[i].y != NULL || list->points[i].z != NULL)) {
+//            printf("인덱스 %d: (x: %d, y: %d, z: %d)\n", i, list->points[i].x, list->points[i].y, list->points[i].z);
+//        }
+//        else {
+//            printf("인덱스 %d: 비어있음\n", i);
+//        }
+//    }
+//}
--- WarmingUp1/question6.c
+++ WarmingUp1/question6.c
@@ -2,127 +2,123 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <stdbool.h>
 #include <conio.h>
 
 #define SIZE 30
 #define PATH 0
 #define BACKGROUND 1
 #define OBSTACLE 'X'
-#define OBJECT '*'
-#define EMPTY ' '
+#define PLAYER '*'
 
-char board[SIZE][SIZE];
-int playerX = -1, playerY = -1;
+int board[SIZE][SIZE];
+int player_x = -1, player_y = -1;  // 플레이어 위치 (-1은 아직 생성되지 않았음을 의미)
+int has_player = 0;
 
-void initializeBoard() {
+// 경로와 장애물 초기화 함수
+void init_board() {
     for (int i = 0; i < SIZE; i++) {
         for (int j = 0; j < SIZE; j++) {
             board[i][j] = BACKGROUND;
         }
     }
-}
-
-void placeObstacles() {
-    int count = 0;
-    while (count < 30) {
+    // 장애물 배치
+    int obstacles = 30;
+    while (obstacles > 0) {
         int x = rand() % SIZE;
         int y = rand() % SIZE;
         if (board[x][y] == BACKGROUND) {
             board[x][y] = OBSTACLE;
-            count++;
+            obstacles--;
         }
     }
 }
 
-bool isValidMove(int x, int y) {
-    return (x >= 0 && x < SIZE && y >= 0 && y < SIZE && board[x][y] != OBSTACLE);
+// 경로 생성 함수 (조건에 맞는 경로)
+void generate_path() {
+    int x = 0, y = 0, steps = 0, direction = rand() % 4;
+    while (x != SIZE - 1 || y != SIZE - 1) {
+        if (steps == 5 || board[x][y] == OBSTACLE) {
+            direction = (direction + rand() % 3 + 1) % 4;  // 다른 방향으로 전환
+            steps = 0;
+        }
+
+        board[x][y] = PATH;  // 경로 표시
+        steps++;
+
+        // 경로 방향에 따른 이동
+        if (direction == 0 && x > 0) x--;         // 위쪽
+        else if (direction == 1 && x < SIZE - 1) x++;  // 아래쪽
+        else if (direction == 2 && y > 0) y--;    // 왼쪽
+        else if (direction == 3 && y < SIZE - 1) y++;  // 오른쪽
+    }
+    board[SIZE - 1][SIZE - 1] = PATH;  // 마지막 지점도 경로로 설정
 }
 
-void drawBoard() {
-    system("clear");
+// 보드 출력 함수
+void print_board() {
     for (int i = 0; i < SIZE; i++) {
         for (int j = 0; j < SIZE; j++) {
-            if (board[i][j] == PATH) {
-                printf("\033[1;33m%d \033[0m", board[i][j]); // 노란색
-            } else if (board[i][j] == BACKGROUND) {
-                printf("%d ", board[i][j]);
-            } else if (board[i][j] == OBSTACLE) {
-                printf("\033[1;34mX \033[0m"); // 파란색
-            } else if (board[i][j] == OBJECT) {
-                printf("\033[1;31m* \033[0m"); // 빨간색
-            } else {
-                printf("  ");
+            if (i == player_x && j == player_y) {
+                printf("\033[1;31m%c \033[0m", PLAYER);  // 빨강색 플레이어
+            }
+            else if (i == SIZE - 1 && j == SIZE - 1) {
+                printf("\033[1;35m* \033[0m");  // 보라색 맨 마지막 지점
+            }
+            else if (board[i][j] == PATH) {
+                printf("\033[1;33m0 \033[0m");  // 노란색 경로
+            }
+            else if (board[i][j] == OBSTACLE) {
+                printf("\033[1;34mX \033[0m");  // 파란색 장애물
+            }
+            else {
+                printf("1 ");
             }
         }
         printf("\n");
     }
 }
 
-void placePath() {
-    // 경로 생성 로직 구현
-    // (조건을 따르도록 작성)
-    // 예시로 간단한 경로를 사용
-    for (int i = 0; i < SIZE; i++) {
-        board[i][0] = PATH;
-    }
-    for (int j = 0; j < SIZE; j++) {
-        board[SIZE - 1][j] = PATH;
-    }
-}
+// 플레이어 이동 함수
+void move_player(char direction) {
+    int new_x = player_x, new_y = player_y;
 
-void resetPath() {
-    initializeBoard();
-    placeObstacles();
-    placePath();
-}
+    if (direction == 'w' && player_x > 0) new_x--;      // 위쪽
+    if (direction == 's' && player_x < SIZE - 1) new_x++;  // 아래쪽
+    if (direction == 'a' && player_y > 0) new_y--;      // 왼쪽
+    if (direction == 'd' && player_y < SIZE - 1) new_y++;  // 오른쪽
 
-void placeObject() {
-    if (playerX == -1 && playerY == -1) {
-        playerX = 0;
-        playerY = 0;
-        board[playerX][playerY] = OBJECT;
-    }
-}
-
-void moveObject(char direction) {
-    int newX = playerX;
-    int newY = playerY;
-
-    switch (direction) {
-        case 'w': newX--; break;
-        case 'a': newY--; break;
-        case 's': newX++; break;
-        case 'd': newY++; break;
-    }
-
-    if (isValidMove(newX, newY) && board[newX][newY] == PATH) {
-        board[playerX][playerY] = BACKGROUND; // 이전 위치 비우기
-        playerX = newX;
-        playerY = newY;
-        board[playerX][playerY] = OBJECT; // 새로운 위치에 객체 배치
+    if (board[new_x][new_y] == PATH) {
+        player_x = new_x;
+        player_y = new_y;
     }
 }
 
 int main() {
-    srand(time(0));
-    initializeBoard();
-    placeObstacles();
-    placePath();
-
+    srand(time(NULL));
     char command;
-    while (true) {
-        drawBoard();
-        command = getch();
 
+    init_board();
+    generate_path();
+
+    while (1) {
+        system("cls");  // 화면 클리어
+        print_board();
+        printf("명령어 입력하세요 (r: 초기화, e: 시작, w/a/s/d: 이동, q: 종료): ");
+        scanf(" %c", &command);  // 명령어 입력 (앞의 공백으로 버퍼 문제 해결)
+
+        if (command == 'q') break;
         if (command == 'r') {
-            resetPath();
-        } else if (command == 'e') {
-            placeObject();
-        } else if (command == 'w' || command == 'a' || command == 's' || command == 'd') {
-            moveObject(command);
-        } else if (command == 'q') {
-            break;
+            init_board();
+            generate_path();
+            has_player = 0;  // 플레이어 삭제
+        }
+        if (command == 'e' && !has_player) {
+            player_x = 0;
+            player_y = 0;
+            has_player = 1;
+        }
+        if (has_player && (command == 'w' || command == 'a' || command == 's' || command == 'd')) {
+            move_player(command);
         }
     }
 
